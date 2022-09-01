8d2a60d137660d2be7e902ce8209236fde2658f0
natnael-tilahun <natnaeltilahun97@gmail.com>
2022-09-01 11:35:18 +0300

    Make the google map link to open on the new tab

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ function card(props) {
         <p className='card--country'>
           <img src="/mapIcon.png" alt='map icon' className='card--map--icon'/>
           {props.location} 
-          <span ><a href={props.googleMapsUrl} className='card--google--map--link'>View on Google Maps</a></span>
+          <span ><a href={props.googleMapsUrl} className='card--google--map--link' target="__blank">View on Google Maps</a></span>
         </p>
         <h2 className='card--location--name'>{props.title}</h2>
         <p className='card--date'>{props.startDate} - {props.endDate}</p>
